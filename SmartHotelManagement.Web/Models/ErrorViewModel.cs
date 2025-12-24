77f314b61321226c7e9abd35416115325f5b3532
Mohammad Ibrahim khalil <ibrahim.cse10@gmail.com>
2025-12-25 02:19:32 +0600

    Add project files.

---
+++ SmartHotelManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHotelManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
